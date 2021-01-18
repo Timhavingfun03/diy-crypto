c5cd36d427bdcf8cdc5797b232f6539c50452f2e
Tim Torndal <tim.torndal@skola.botkyrka.se>
2021-01-18 14:36:23 +0100

    encoding

---
+++ name.js
@@ -0,0 +1,26 @@
+
+
+function caesarEncrypt(msg, shift){
+
+const charCodes = []
+for (let i = 0; i < secretMessage.length; i++) {
+    charCodes.push(msg[i].charCodeAt(0))
+}
+
+for (let i = 0; i < charCodes.length; i++) {
+    charCodes[i] = charCodes[i] + shift
+}
+
+let result = ""
+for (let i = 0; i < charCodes.length; i++) {
+    result = result + String.fromCharCode(charCodes[i])
+    
+    }
+
+    return result
+}
+
+const secretMessage = "RETREATTOMORROW"
+const encryptedMessage = caesarEncrypt(secretMessage, 5)
+
+console.log(secretMessage, encryptedMessage)
